1431fa0afc00f61a4a8f2f2c45e75014c4555256
vpeker <guruautomation04@gmail.com>
2020-10-10 21:07:29 -0600

    first commit

--- src/main/java/PageFactory/HomePage.java
+++ src/main/java/PageFactory/HomePage.java
@@ -9,36 +9,34 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePage {
 //    WebDriver homePageDriver;
 
-        public HomePage(WebDriver driver) {
+    public HomePage(WebDriver driver) {
 //        homePageDriver = driver;
+        PageFactory.initElements(driver,this);
+    }
 
-            PageFactory.initElements(driver, this);
-        }
+    @FindBy(how = How.XPATH, using = "//img[@alt=\"Let's Kode It\"]")
+    public static WebElement homeImg;
 
-        @FindBy(how = How.XPATH, using = "//img[@alt=\"Let's Kode It\"]")
-        public static WebElement homeImg;
+    //a[@href='/pages/practice']
+    @FindBy(how = How.XPATH, using = "//a[contains(text(),'Practice')]")
+    public static WebElement practiceBtn;
 
-        //a[@href='/pages/practice']
-        @FindBy(how = How.XPATH, using = "//a[contains(text(),'Practice')]")
-        public static WebElement practiceBtn;
+    @FindBy(how = How.XPATH, using = "//a[@href='/sign_in']")
+    public static WebElement enrolNow;
 
-        @FindBy(how = How.XPATH, using = "//a[@href='/sign_in']")
-        public static WebElement enrolNow;
-
-        public void clickPractice() {
-            practiceBtn.click();
-        }
+    public void clickPractice(){
+        practiceBtn.click();
+    }
 
 
-        public void validateHomeScr(String expSrc) {
-            String actualSrc = homeImg.getAttribute("src");
-            assert (actualSrc.equals(expSrc));
+    public void validateHomeScr(String expSrc){
+        String actualSrc = homeImg.getAttribute("src");
+        assert(actualSrc.equals(expSrc));
 //https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=height:120/https://www.filepicker
 // .io/api/file/WSundQ6BT6u2OZe2kcNA 2x
-        }
+    }
 
-        public void isEnrollNowDisplayed() {
-            enrolNow.isDisplayed();
-        }
-
+    public void isEnrollNowDisplayed(){
+        enrolNow.isDisplayed();
+    }
     }
